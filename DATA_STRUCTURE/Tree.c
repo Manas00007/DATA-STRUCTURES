e0822a5968a113f4ab7caff87ffbb0304d0c2035
Manas Pokhriyal <manaspokhriyal911@gmail.com>
2025-11-18 22:12:35 +0530

    deletiom operation added but tesing is still remaining

--- DATA_STRUCTURE/Tree.c
+++ DATA_STRUCTURE/Tree.c
@@ -34,8 +34,8 @@ tree* traverse(int n ,tree* temp){
     }
     if(temp->data==n)
     {
-        printf("already found");
-        return temp;
+        printf("already exsists\n");
+        return NULL;
     }
     
         if(temp->data>n)
@@ -77,13 +77,14 @@ void insertion()
     printf("Enter data: ");
     scanf("%d",&n);
 
-    tree* new=(tree*) malloc(sizeof(tree));
 
     tree* parent=traverse(n,root);
     if(parent==NULL)
     {
         return;
     }
+    tree* new=(tree*) malloc(sizeof(tree));
+
 
 
     new->data=n;
@@ -100,6 +101,222 @@ void insertion()
     }
 
 }
+
+
+    tree* find_node(int n,tree* temp)
+    {
+        if(temp==NULL)
+        {
+            return NULL;
+        }
+
+        if((temp->left !=NULL && temp->left->data==n)|| (temp->right !=NULL &&temp->right->data==n))
+        {
+            return temp;
+        }
+
+        if(temp->data>n)
+        {
+            if(temp->left==NULL)
+            {
+                return NULL;
+            }
+            else
+            {
+                return find_node(n,temp->left);
+            }
+        }
+        else
+        {
+            if(temp->right==NULL)
+            {
+                return NULL;
+            }
+            else 
+            {
+                return find_node(n,temp->right);
+            }
+        }
+
+
+    }
+
+
+    void deletion()
+    {
+        if(root==NULL)
+        {
+            printf("node is not available\n");
+            return;
+
+        }
+
+        int n;
+        printf("Enter node data to delete: ");
+        scanf("%d",&n);
+
+        if(root->data==n)
+        {
+            tree* temp=root;
+            if(root->left!=NULL && root->right==NULL)
+            {
+               root=root->left;
+               free(temp); 
+            }
+            else if(root->left==NULL && root->right!=NULL)
+            {
+                root=root->right;
+                free(temp);
+            }
+            else if(root->left==NULL && root->right==NULL)
+            {
+                root=NULL;
+                free(temp);
+            }
+            else
+            {
+                tree* pred_parent=root;
+                tree* pred=temp->left;
+
+                while(pred->right!=NULL)
+                {
+                    pred_parent=pred;
+                    pred=pred->right;
+                }
+                root->data=pred->data;
+
+                if(pred->left!=NULL)
+                {
+                    pred_parent->right=pred->left;
+                   
+                }
+                else
+                {
+                    pred_parent->right=NULL;
+
+                }
+                free(pred);
+            }
+
+            return;
+        }
+
+        tree* parent=find_node(n,root);  //parent node of node  to delete
+        tree* dnode;  // node to delete
+        
+
+
+        if(parent->left->data>n)
+        {
+            dnode=parent->left;
+        
+
+        if(dnode->left!=NULL && dnode->right==NULL)
+        {
+            parent->left=dnode->left;
+            free(dnode);
+        }
+        else if(dnode->left==NULL && dnode->right!=NULL)
+        {
+            parent->left=dnode->right;
+            free(dnode);
+        }
+        else if(dnode->left==NULL && dnode->right==NULL)
+        {
+            parent->left=NULL;
+            free(dnode);
+        }
+        else
+        {
+            
+                tree* pred_parent=dnode;
+                tree* pred=dnode->left;
+
+                while(pred->right!=NULL)
+                {
+                    pred_parent=pred;
+                    pred=pred->right;
+                }
+
+                dnode->data=pred->data;
+
+                if(pred->left!=NULL)
+                {
+                    pred_parent->right=pred->left;
+                }
+                else
+                {
+                    pred_parent->right=NULL;
+                }
+                free(pred);
+
+
+        }
+        }
+
+        else
+        {
+            dnode=parent->right;
+
+        if(dnode->left!=NULL && dnode->right==NULL)
+        {
+            parent->left=dnode->left;
+            free(dnode);
+        }
+        else if(dnode->left==NULL && dnode->right!=NULL)
+        {
+            parent->left=dnode->right;
+            free(dnode);
+        }
+        else if(dnode->left==NULL && dnode->right==NULL)
+        {
+            parent->left=NULL;
+            free(dnode);
+        }
+        else
+        {
+            
+                tree* pred_parent=dnode;
+                tree* pred=dnode->left;
+
+                while(pred->right!=NULL)
+                {
+                    pred_parent=pred;
+                    pred=pred->right;
+                }
+
+                dnode->data=pred->data;
+
+                if(pred->left!=NULL)
+                {
+                    pred_parent->right=pred->left;
+                }
+                else
+                {
+                    pred_parent->right=NULL;
+                }
+                free(pred);
+
+
+        }
+    }
+    }
+
+
+    
+
+
+
+
+tree* small(tree* temp)
+{
+    if(temp->left==NULL)
+    {
+        return temp;
+    }
+    return small(temp->left);;
+}
+
 
 
 void preorder(tree* temp)
@@ -154,6 +371,8 @@ void display()
     printf("Postorder: ");
     postorder(root);
 }
+
+
 int main()
 {
 
@@ -165,7 +384,19 @@ int main()
 
     insertion();
     insertion();
+    insertion();
+    insertion();
+    insertion();
+    insertion();
+    insertion();
+    insertion();
 
+
+    display();
+
+    deletion();
+
+    printf("\n");
     display();
     return 0;
 }
