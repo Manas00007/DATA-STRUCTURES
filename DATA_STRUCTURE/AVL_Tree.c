901e8b7c4f681b209c6a2d0a3b9c80deea49cf60
Manas Pokhriyal <manaspokhriyal911@gmail.com>
2025-12-10 23:15:57 +0530

    updation in code

--- DATA_STRUCTURE/AVL_Tree.c
+++ DATA_STRUCTURE/AVL_Tree.c
@@ -13,181 +13,58 @@ typedef struct node{
 
 avl* root=NULL;
 
-
-
-avl* traverse(int n,avl* temp)
+avl* get_height(avl* temp)
 {
+    if(temp==NULL)
+    {
+        return 0;
+    }
+    return temp->height;
+}
 
 
-    if(temp->right==NULL && temp->left==NULL)
+void update_height(avl* temp,int n)
+{
+    avl* temp2;
+    if( temp->data==n)
     {
-        return temp;
-    }
-    if(temp->data==n){
-        printf("\nPlase don't enter duplicate data \n");
-        return NULL;
-    }
-    if((temp->left!=NULL && temp->left==n)||(temp->right!=NULL && temp->left==n))
-    {
-        return NULL;
+        return;
     }
 
     if(temp->data>n)
     {
-        return traverse(n,temp->left);
+        update_height(temp->left,n);
+        temp->height=(get_height(temp->left)-get_height(temp->right))+1;
+        check_fix_balance(temp,n);
+        
     }
     else
     {
-        return traverse(n,temp->right);
+        update_height(temp->right,n);
+        temp->height=(get_height(temp->left)-get_height(temp->right))+1;
     }
-
 
 }
 
-int maximum(avl* left,avl* right)
+void check_fix_balance(avl* temp,int n)
 {
-    if(left==NULL && right==NULL)
+    int left_height=get_height(temp->left);
+    int right_height=get_height(temp->right);
+    int diff=left_height-right_height;
+
+    if(diff>=2 || diff<=-2)
     {
-        return -1;
-    }
-    else if(left!=NULL && right==NULL)
-    {
-        return left->height;
-    }
-    else if(left==NULL && right!=NULL)
-    {
-        return right->height;
-    }
-    else
-    {
-        if(left->height>right->height)
+        if(diff>=2)
         {
-            return left->height;
+            // left left or left right rotation
 
         }
         else
         {
-            return right->height;
+            // right right or right left rotation 
         }
     }
-}
 
-
-void update_height(avl* temp)//update the height of tree
-{
-
-    if(temp==NULL)
-    {
-        return;
-    }
-
-    update_height(temp->left);
-    update_height(temp->right);
-    temp->height=1+maximum(temp->left,temp->right);
-    return;
-
-}
-
-//this function check balance of tree and if it is unbalance it balance the tree
-void check_fix_balance(avl* temp,int n)   //temp = root refrenece
-{
-    avl* r=temp;
-
-    if(temp->data==n)
-    {
-        return;
-    }
-
-    if(temp->data>n)
-    {
-        check_fix_balance(temp->left,n);
-    }
-    else
-    {
-        check_fix_balance(temp->right,n)
-    }
-
-    if(temp->left->height==NULL && temp->right->height=!NULL)
-    {
-        int x=-1;
-        int y=temp->right->height;
-
-    }
-    else if(temp->left->height!=NULL && temp->right->height==NULL)
-    {
-        int x=temp->left->height;
-        int y=-1
-    }
-    else
-    {
-    int x=temp->left->height;
-    int y=temp->right->height;
-    }
-
-    int z=x-y;
-    if(z>=2 || z<=-2)
-    {
-        avl* parent = rotate(temp,z,r); // this function rotate and return new parent node for height updation....
-        update_height(parent);
-    }
-    return;
-}
-
-avl* parent(int n,avl* r)
-{
-
-    if((r->left!=NULL && r->left->data==n) || (r->right!=NULL && r->right->data==n))
-    {
-        return r;
-    }
-
-    if(r->data>n)
-    {
-        return parent(n,r->left);
-
-    }
-    else
-    {
-        return parent(n,r->right);  
-    }
-    
-}
-
-avl* rotate(avl* temp, int n,avl* r);
-{
-    avl* parent=parent(temp->data,r);
-    if(n>-1)
-    {
-        avl* child=temp->left->right;
-        
-        // left subtree
-        if(temp->left->left==NULL && temp->left->right!=NULL)//LR rotation
-        {
-            child->left=temp->left;
-            temp->left->right=NULL;
-            temp->left=child;
-
-        }
-            //LL rotation
-            child->right=temp;
-            temp->left=NULL;
-            parent->left=child;
-
-
-
-        
-    }
-    else
-    {
-        //right subtree 
-        if(temp->right->right!=NULL)
-        {
-            //RL rotaion
-        }
-       
-            //RR rotation 
-
-    }
 }
 
 void insert()
@@ -222,9 +99,7 @@ void insert()
             temp->right=new;
         }   
     }
-    update_height(root);
-
-    check_fix_balance(root,n);
+    update_height(root,n);
 
 
 }
