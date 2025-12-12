d79be813a0a47747c5d87fd74c47591bae95b307
Manas Pokhriyal <manaspokhriyal911@gmail.com>
2025-12-12 23:37:59 +0530

    fix code logic and bug fixes

--- DATA_STRUCTURE/AVL_Tree.c
+++ DATA_STRUCTURE/AVL_Tree.c
@@ -3,7 +3,9 @@
 #include<stdbool.h>
 
 
-typedef struct node{
+
+typedef struct node
+{
     int data;
     int height;
     struct node* left;
@@ -11,250 +13,105 @@ typedef struct node{
 }avl;
 
 
-avl* root=NULL;
+avl* root;
 
-avl* get_height(avl* temp)
+
+void create()
+{
+
+    int n;
+    printf("Enter data:");
+    scanf("%d",&n);
+
+    avl* new=(avl*) malloc(sizeof(avl));
+
+    new->data=n;
+    new->left=NULL;
+    new->right=NULL;
+    new->height=1;
+    root=new;
+}
+
+int get_height(avl* temp)
 {
     if(temp==NULL)
     {
         return 0;
     }
+
     return temp->height;
 }
 
-
-void update_height(avl* temp,int n)
+avl* traverse(avl* temp,int n)
 {
-    if( temp->data==n)
-    {
-        return;
-    }
-
-    if(temp->data>n)
-    {
-        update_height(temp->left,n);
-        temp->height=(get_height(temp->left)-get_height(temp->right))+1;
-        check_fix_balance(temp,n);
-        
-    }
-    else
-    {
-        update_height(temp->right,n);
-        temp->height=(get_height(temp->left)-get_height(temp->right))+1;
-        check_fix_balance(temp,n);
-
-    }
-
-}  
-
-
-avl* superParent(int n,avl* temp)
-{
-    if(temp==NULL)
+    if(temp=NULL)
     {
         return NULL;
     }
-
-
-    if(temp->data>n)
-    {
-        if(temp->left->data==n)
-        {
-            return temp;
-        }
-        else
-        {
-        return superParent(n,temp->left);
-        }
-    }
-    else
-    {
-         if(temp->right->data==n)
-        {
-            return temp;
-        }
-        else
-        {
-        return superParent(n,temp->right);
-        }
-    }
-
-}   
-
-void LL(avl* parent)
-{
-    avl* grand=superParent(parent->data,root);
-    avl* child=temp->left;
-
-    grand->left=child;
-
-    if(child->right!=NULL)
-    {
-        avl* temp=child->right;
-        parent->left=temp;
-    }
-    child->right=parent;
-
-    update_height(root,child->data);
-
-}
-
-void LR(avl* parent)
-{
-
-    avl* child=parent->left;
-    avl* temp=child->right;
-
-    parent->left=temp;
-    temp->left=child;
-
-    LL(parent);
-
-
-
-}
-
-
-void RR(avl* parent)
-{
-    avl* grand=superParent(parent->data,root);
-    avl* child=parent->right;
-
-    grand->right=child;
-
-    if(child->left!=NULL)
-    {
-        avl* temp=child->left;
-        parent->right=temp;
-    }
-
-    child->left=parent;
-
-}
-
-
-void RL(avl* parent)
-{
-
-    avl* child=parent->right;
-    avl* temp=child->left;
-    parent->right=temp;
-    temp->right=child;
-
-    RR(parent);
-
-}
-
-void check_fix_balance(avl* temp,int n)
-{
-    int left_height=get_height(temp->left);
-    int right_height=get_height(temp->right);
-    int diff=left_height-right_height;
-
-    if(diff>=2 || diff<=-2)
-    {
-        if(diff>=2)
-        {
-            avl* child=temp->left;
-            if(child->left==NULL)
-            {
-                LR(temp);
-            }
-            else
-            {
-                LL(temp);
-            }
-
-            // left left or left right rotation
-
-        }
-        else
-        {
-            avl* child=temp->left;
-            if(child->right==NULL)
-            {
-                RL(temp);
-            }
-            else
-            {
-                RR(temp);
-            }
-            // right right or right left rotation 
-        }
-    }
-
-}
-
-
-avl* traverse(int n, avl* temp)
-{
-
-    if(temp==NULL)
-    {
-        return NULL;
-    }
-
     if(temp->data==n)
     {
-        printf("Already exist!");
+        printf("Already Exist!");
         return NULL;
-
     }
-
-
-
 
     if(temp->data>n)
     {
-
         if(temp->left==NULL)
         {
             return temp;
         }
-        else{
-
-         return traverse(n,temp->left);
-    }
-         
+        else
+        {
+            return traverse(temp->left,n);
+        }
     }
     else
     {
-        if(temp->right==NULL)
+          if(temp->right==NULL)
         {
             return temp;
         }
         else
         {
-        return traverse(n,temp->right);
+            return traverse(temp->right,n);
         }
-        
     }
+}
 
 
+int balance_factor(avl* temp)
+{
+    return (get_height(temp->left)-get_height(temp->right));
+}
 
+int max(int a,int b)
+{
+    return (a>b)?a:b;
+}
+
+int update_height(avl* temp)
+{
+    return max(get_height(temp->left),get_height(temp->right))+1;
 }
 
 void insert()
 {
-
-    int n;
-    printf("Enter data: ");
-    scanf("%d",&n);
-
-    avl* temp=traverse(n,root);
-
-    avl* new=(avl*) malloc(sizeof(avl));
- 
-    new->data=n;
-    new->left=NULL;
-    new->right=NULL;
-    new->height=0;
-
     if(root==NULL)
     {
-        root=new; 
+        create();
+        return;
     }
 
+    avl* new=(avl*) malloc(sizeof(avl));
+    int n;
+    printf("\n Enter data: ");
+    scanf("%d",&n);
+    new->data=n;
+    new->height=1;
+    new->left=NULL;
+    new->right=NULL;
+
+    avl* temp=traverse(root,n);
     if(temp!=NULL)
     {
         if(temp->data>n)
@@ -264,12 +121,31 @@ void insert()
         else
         {
             temp->right=new;
-        }   
+        }
     }
-    update_height(root,n);
+     root->height=update_height(root);
 
+        int bf=balance_factor(root);
+
+        if(bf>1 && root->left->data>n)
+        {
+            //LL rotation
+        }
+        if(bf<-1 && root->right->data<n)
+        {
+            //RR rotation
+        }
+        if(bf>1 && root->left->data<n)
+        {
+            //LR rotation
+        }
+         if(bf<-1 && root->right->data>n)
+        {
+            //RL rotation
+        }
 
 }
+
 int main()
 {
     return 0;
