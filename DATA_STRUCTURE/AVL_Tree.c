1f7bb08b12e980f967a2a05ff9231f29a4079ff2
Manas Pokhriyal <manaspokhriyal911@gmail.com>
2025-12-13 23:47:55 +0530

    code optimization

--- DATA_STRUCTURE/AVL_Tree.c
+++ DATA_STRUCTURE/AVL_Tree.c
@@ -129,14 +129,52 @@ void insert()
 
         if(bf>1 && root->left->data>n)
         {
-            //LL rotation
+            avl* child=root->left;
+            avl temp=root;
+
+            if(child->right!=NULL)
+            {
+                avl* temp2=child->right;
+                temp->left=temp2;
+                temp2->height=max(get_height(temp2->left),get_height(temp2->right))+1;
+            }
+            child->right=temp;
+            temp->height=max(get_height(temp->left),get_height(temp->right))+1;
+            child->height=max(get_height(child->left),get_height(child->right))+1;
+            root=child;
+            return;
+
+            
+             //LL rotation
         }
         if(bf<-1 && root->right->data<n)
         {
+            avl* child=root->right;
+            avl temp=root;
+            if(child->left!=NULL)
+            {
+                avl* temp2=child->left;
+                temp->right=temp2;
+                temp2->height=max(get_height(temp2->left),get_height(temp2->right))+1;
+            }
+            child->left=temp;
+            temp->height=max(get_height(temp->left),get_height(temp->right))+1;
+            child->height=max(get_height(child->left),get_height(child->right))+1;
+            root=child;
+            return;
+            
             //RR rotation
         }
         if(bf>1 && root->left->data<n)
         {
+            avl* child=root->left;
+            avl* grandchild=child->right;
+            avl* temp=root;
+            temp->left=grandchild;
+            grandchild->left=child;
+            grandchild->right=temp;
+
+
             //LR rotation
         }
          if(bf<-1 && root->right->data>n)
